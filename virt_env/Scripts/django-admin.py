7e38860e996c653ff39cff5fa1e3dde418f1ec1a
VGT GAYAN <gayanvgt@gmail.com>
2020-10-18 12:20:40 +0530

    second commit with initial files

---
+++ virt_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gayan\documents\personal\my\dev\python\django\trydjango\virt_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
